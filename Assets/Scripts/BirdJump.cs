92178a417bffd280a81daaa26a02a9faf9ac5dbb
leechuanhung <163072144+leechuanhung@users.noreply.github.com>
2024-03-19 12:42:03 +0900

    플래피 버드 완성

--- Assets/Scripts/BirdJump.cs
+++ Assets/Scripts/BirdJump.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdJump : MonoBehaviour
 {
     Rigidbody2D rb;
-
+    AudioSource audioSource;
     public float JumpPower;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -18,10 +20,20 @@ public class BirdJump : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * JumpPower;
+            audioSource.Play();
         }
        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = Vector2.up * JumpPower;
+            audioSource.Play();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene("GameOverScene");
+        if(Score.score > Score.bestscore)
+        {
+            Score.bestscore = Score.score;
         }
     }
 }
